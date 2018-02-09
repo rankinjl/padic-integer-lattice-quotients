8f6981eb017fa12fad4acebcc40f3993c3f1bf92
Jessica Rankins <jessicarankins18@gmail.com>
2018-02-09 12:50:41 -0500

    Allow p-adic matrices and same prime & precision for entire session

---
+++ PadicMatrix.py
@@ -0,0 +1,87 @@
+#By Jessica Rankins Spring 2018
+from PadicNumber import *
+
+class PadicMatrix:
+    """PadicMatrix: holds p-adic numbers in an nxm matrix and allows
+    different operations to be done on this matrix"""
+
+    ''' private instance attributes for a PadicMatrix:
+        rows: the (integer) number of rows in the matrix
+        columns: the (integer) number of columns in the matrix
+        values: the actual PadicNumbers in each row and column represented
+            as an array of arrays
+            []->[],[],...,[]
+            []->[],[],...,[]
+            :       :     
+            []->[],[],...,[]
+    '''
+
+    #Pre: need to create a rowsXcolumns matrix of p-adic numbers given by
+        #valueListRowByRow (listed as row1 elements followed by row2, etc)
+    #Post: PadicMatrix created
+    def __init__(self, rows, columns, valueListRowByRow=[]):
+        if(not isinstance(rows,int) or rows<=0):
+            raise ValueError("Rows = "+str(rows)+" is not a positive integer!")
+        self.__rows = int(rows)
+        if(not isinstance(columns,int) or columns<=0):
+            raise ValueError("Columns = "+str(columns)+" is not a positive integer!")
+        self.__columns = int(columns)
+        self.__values = self.__fillInValues(valueListRowByRow)
+
+    #Pre: need to fill in the values for this PadicMatrix given in valueList
+    #Post: values assigned
+    def __fillInValues(self,valueList):
+        rowsAndCols = [[]]*self.__rows
+        if(len(valueList)==0):
+            #fill in with zeros
+            for r in range(self.__rows):
+                rowsAndCols[r] = [PadicNumber([],0)]*self.__columns
+        elif(len(valueList)!=(self.__rows*self.__columns)):
+            raise ValueError("The number of values does not match the number of entries in the matrix!")
+        else:
+            #fill in rowsAndCols with valueList values where
+            #rowsAndCols[r][c] = valueList[(r-1)*self.__columns+c]
+            for r in range(self.__rows):
+                rowsAndCols[r] = [0]*self.__columns
+                for c in range(self.__columns):
+                    value = valueList[(r-1)*self.__columns+c]
+                    if(not isinstance(value,PadicNumber)):
+                        raise ValueError("Values must be PadicNumbers!")
+                    rowsAndCols[r][c] = value
+        return rowsAndCols
+
+    #Pre: need to get the value at this row and this column
+    #Post: value at row row and column column returned
+    def getValue(self, row, column):
+        return self.__values[row][column]
+
+    #Pre: need to get all the values in this PadicMatrix
+    #Post: list of lists containing the values returned accessed [r][c]
+    def getValues(self):
+        return self.__values
+
+    #Pre: need to get the number of rows in this PadicMatrix
+    #Post: number of rows returned
+    def getRows(self):
+        return self.__rows
+
+    #Pre: need to get the number of columns in this PadicMatrix
+    #Post: number of columns returned
+    def getColumns(self):
+        return self.__columns
+
+
+    #Pre: need to print this p-adic matrix
+    #Post: string describing p-adic matrix returned
+    def __str__(self):
+        description = "[\n"
+        for r in range(self.__rows):
+            description = description + " row "+str(r+1)+": [\n"
+            for c in range(self.__columns):
+                description = description +"\t\t"+str(self.__values[r][c])+"\n"
+            description = description + "\t]\n"
+        description = description + "]"
+        return description
+
+
+
---
+++ RunMe.py
@@ -0,0 +1,48 @@
+#from PadicNumber import *
+from PadicMatrix import *
+
+#Pre: need to display helpful tips to user
+#Post: helpful tips displayed to user
+def displayHelp():
+    print("Welcome to the p-adic number program!")
+    print("To create a p-adic number, use [varName = ] PadicNumber(list_of_coefficients,smallest_power)")
+    print("For example, 'one = Padic([1],0)' or 'Padic([1,1,0,0,1),-3)")
+    print("For Padics a and b, you can print(a), a.multiply(b), a.add(b), "+
+          "a.getAdditiveInverse(), b.getMultiplicativeInverse, b.divide(a), a.subtract(b), etc")
+    print()
+
+    
+#Pre: need to know if number is prime
+#Post: True returned if number is prime, false otherwise
+def isPrime(number):
+    if(number<=1):
+        return False
+    else:
+        flag = True
+        for i in range(2,number):
+            if number%i==0:
+                flag = False
+        return flag
+
+#Pre: introduce the user to the p-adic program and allow them to set prime
+    #and precision
+#Post: help displayed, prime and precision set
+def main():    
+    displayHelp()
+    prime = 0
+    precision = 0
+    while(not isPrime(prime)):
+        try:
+            prime = int(input("What prime would you like to use?"))
+        except:
+            pass
+    while(precision<=0):
+        try:
+            precision = int(input("What precision would you like to use?"))
+        except:
+            pass
+    PadicNumber.setPrimePrecision(prime,precision)
+
+
+
+main()
